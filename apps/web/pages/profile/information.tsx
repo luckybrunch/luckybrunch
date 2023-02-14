8421d0a39f7a3ac5ac0ab8d78fb5a7116699b80b
jhonadc <jhonathanaugusto@gmail.com>
2023-02-14 15:41:43 +0100

    Campos/eng 68 dublicate the profile backend logic from (#10)
    * simplest schema change on the db
    
    * env inserted. schema db changed to avoid not null error, must check with emil
    
    * database migration working
    
    * comments added to structure the backend on info page
    
    * changed schema.prisma following previous LB db design
    
    * migration successful
    
    * corrected typo on companyName field on User Model
    
    * migration completed - typo corrected
    
    * updated defaultValues followind db Model names and inserted onSubmit mutation reference
    
    * changed email as avatar fallback method to the user name, since no email on info page
    
    * changed avatar size to large
    
    * error turndown service on bio input form corrected
    
    * turndown import added
    
    * updated information page with comments to control code changes
    
    * added new user properties to viewer.tsx loggedInViewerRouter
    
    * added new user properties to createContext.ts getUserFromSession query
    
    * corrected typo on viewer.tsx
    
    * corrected typo on information.tsx
    
    * further name changes to information page
    
    * backup before reinserting deleted cal.com code to fine tune mutation not working
    
    * deleted unused code and corrected prisma schema so zip code is set to Integer
    
    * inserted new mutations types on viewer-mutations - should work now -new error Two sidebars loading
    
    * deleted alternative empty string to zip number type
    
    * duplicate sidebar corrected - error message corrected when not updating data
    
    * IT WORKS! sending data to prisma sucessfully. zip must be set to string. implement form restriction on the front end
    
    * cleaned the code and tested. still works
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Emil <emil@twigbit.com>

--- apps/web/pages/profile/information.tsx
+++ apps/web/pages/profile/information.tsx
@@ -2,15 +2,12 @@ import { DesktopComputerIcon, HomeIcon, CheckIcon } from "@heroicons/react/solid
 import { IdentityProvider } from "@prisma/client";
 import crypto from "crypto";
 import MarkdownIt from "markdown-it";
-import { GetServerSidePropsContext } from "next";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 
 import Shell from "@calcom/features/shell/Shell";
-import { ErrorCode } from "@calcom/lib/auth";
-import { APP_NAME } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import turndownService from "@calcom/lib/turndownService";
+import turndown from "@calcom/lib/turndownService";
 import { trpc } from "@calcom/trpc/react";
 import {
   Avatar,
@@ -29,15 +26,11 @@ import {
   Checkbox,
 } from "@calcom/ui";
 
-import { ssrInit } from "@server/lib/ssr";
-
 const md = new MarkdownIt("default", { html: true, breaks: true });
 
-//check Emil, keep or modify?:
-const SkeletonLoader = ({ title, description }: { title: string; description: string }) => {
+const SkeletonLoader = () => {
   return (
     <SkeletonContainer>
-      <Meta title={title} description={description} />
       <div className="mt-6 mb-8 space-y-6 divide-y">
         <div className="flex items-center">
           <SkeletonAvatar className="h-12 w-12 px-4" />
@@ -55,14 +48,16 @@ const SkeletonLoader = ({ title, description }: { title: string; description: st
 
 type FormValues = {
   avatar: string;
-  first_name: string;
-  last_name: string;
-  company_name: string;
-  biography: string;
-  street_name_number: string; //change to allow numbers
-  zip_code: number;
-  city: string; //ort translation correct?
-  to_be_inputed_later: string; // check design update later on
+  name: string;
+  email: string;
+  companyName: string;
+  bio: string;
+  addressLine1: string;
+  addressLine2: string;
+  zip: string;
+  city: string;
+  country: string;
+  // check design update later on for specilization fields
 };
 
 const InformationPage = () => {
@@ -75,56 +70,42 @@ const InformationPage = () => {
       showToast(t("settings_updated_successfully"), "success");
       utils.viewer.me.invalidate();
       utils.viewer.avatar.invalidate();
-      setTempFormValues(null);
     },
     onError: () => {
       showToast(t("error_updating_settings"), "error");
     },
   });
 
-  const [tempFormValues, setTempFormValues] = useState<FormValues | null>(null);
-
-  const isCALIdentityProviver = user?.identityProvider === IdentityProvider.CAL;
-
-  const errorMessages: { [key: string]: string } = {
-    [ErrorCode.SecondFactorRequired]: t("2fa_enabled_instructions"),
-    [ErrorCode.IncorrectPassword]: `${t("incorrect_password")} ${t("please_try_again")}`,
-    [ErrorCode.UserNotFound]: t("no_account_exists"),
-    [ErrorCode.IncorrectTwoFactorCode]: `${t("incorrect_2fa_code")} ${t("please_try_again")}`,
-    [ErrorCode.InternalServerError]: `${t("something_went_wrong")} ${t("please_try_again_and_contact_us")}`,
-    [ErrorCode.ThirdPartyIdentityProviderEnabled]: t("account_created_with_identity_provider"),
-  };
-
   if (isLoading || !user || isLoadingAvatar || !avatar)
     return (
-      <SkeletonLoader
-        title={t("Information")}
-        description={t("Information description", { appName: APP_NAME })}
-      />
+      <Shell title={t("lb_information_title")} subtitle={t("lb_information_subtitle")}>
+        <SkeletonLoader />
+      </Shell>
     );
 
-  //correct the name from database later on => first_name = user.first_name
   const defaultValues = {
     avatar: avatar.avatar || "",
-    first_name: user.name || "",
-    last_name: user.name || "",
-    company_name: user.name || "",
-    biography: user.name || "",
-    street_name_number: user.name || "",
-    zip_code: user.name || "",
-    city: user.name || "",
+    name: user.name || "",
+    email: user.email || "",
+    bio: user.bio || "",
+    companyName: user.companyName || "",
+    addressLine1: user.addressLine1 || "",
+    addressLine2: user.addressLine2 || "",
+    zip: user.zip || "",
+    city: user.city || "",
+    country: user.country || "",
   };
 
   return (
-    <>
-      <Meta
-        title={t("lb_information_title")}
-        description={t("lb_information_subtitle", { appName: APP_NAME })}
+    <Shell heading={t("lb_information_title")} subtitle={t("lb_information_subtitle")}>
+      <ProfileForm
+        key={JSON.stringify(defaultValues)}
+        defaultValues={defaultValues}
+        onSubmit={(values) => {
+          mutation.mutate(values);
+        }}
       />
-
-      <ProfileForm key={JSON.stringify(defaultValues)} defaultValues={defaultValues} />
-      <hr className="my-6 border-gray-200" />
-    </>
+    </Shell>
   );
 };
 
@@ -139,7 +120,7 @@ const ProfileForm = ({
 
   const emailMd5 = crypto
     .createHash("md5")
-    .update(defaultValues.first_name || "example@example.com")
+    .update(defaultValues.email || "example@example.com")
     .digest("hex");
 
   const formMethods = useForm<FormValues>({
@@ -153,142 +134,100 @@ const ProfileForm = ({
   const isDisabled = isSubmitting || !isDirty;
 
   return (
-    <Shell title={t("lb_information_title")}>
-      <Form form={formMethods} handleSubmit={onSubmit}>
-        <div className="flex items-center">
-          <Controller
-            control={formMethods.control}
-            name="avatar"
-            render={({ field: { value } }) => (
-              <>
-                <Avatar alt="" imageSrc={value} gravatarFallbackMd5={emailMd5} size="md" />
+    <Form form={formMethods} handleSubmit={onSubmit}>
+      <div className="flex items-center">
+        <Controller
+          control={formMethods.control}
+          name="avatar"
+          render={({ field: { value } }) => (
+            <>
+              <Avatar alt="" imageSrc={value} gravatarFallbackMd5={emailMd5} size="lg" />
 
-                <div className="ltr:ml-4 rtl:mr-4">
-                  {/* button color is already set inside component */}
-                  <ImageUploader
-                    target="avatar"
-                    id="avatar-upload"
-                    buttonMsg={t("choose_a_file")}
-                    handleAvatarChange={(newAvatar) => {
-                      formMethods.setValue("avatar", newAvatar, { shouldDirty: true });
-                    }}
-                    imageSrc={value || undefined}
-                  />
-                </div>
-              </>
-            )}
-          />
+              <div className="ltr:ml-4 rtl:mr-4">
+                {/* button color is already set inside component */}
+                <ImageUploader
+                  target="avatar"
+                  id="avatar-upload"
+                  buttonMsg={t("choose_a_file")}
+                  handleAvatarChange={(newAvatar) => {
+                    formMethods.setValue("avatar", newAvatar, { shouldDirty: true });
+                  }}
+                  imageSrc={value || undefined}
+                />
+              </div>
+            </>
+          )}
+        />
+      </div>
+      <div className=" lg:flex">
+        <div className="mt-8 lg:w-2/4">
+          <TextField className="lg:w-full" label={t("full_name")} {...formMethods.register("name")} />
         </div>
-        <div className=" lg:flex">
-          {" "}
-          <div className="mt-8 lg:w-2/4">
-            <TextField
-              className="lg:w-full"
-              label={t("lb_first_name")}
-              {...formMethods.register("first_name")}
-            />
-          </div>
-          <div className="mt-8 lg:ml-2 lg:w-2/4">
-            <TextField
-              className="lg:w-full"
-              label={t("lb_last_name")}
-              {...formMethods.register("last_name")}
-            />
-          </div>
-        </div>
+      </div>
 
-        <div className="mt-8 lg:mt-2">
-          <TextField label={t("lb_company_name")} {...formMethods.register("company_name")} />
+      <div className="mt-8 lg:mt-2">
+        <TextField label={t("lb_company_name")} {...formMethods.register("companyName")} />
+      </div>
+      <div className="mt-8 lg:mt-2">
+        <Label>{t("lb_biography")}</Label>
+        <Editor
+          getText={() => md.render(formMethods.getValues("bio") || "")}
+          setText={(value: string) => {
+            formMethods.setValue("bio", turndown(value), { shouldDirty: true });
+          }}
+          excludedToolbarItems={["blockType"]}
+        />
+      </div>
+      <div className="mt-8">
+        <h4 className="text-lg font-bold">{t("lb_work_address")}</h4>
+        <h6 className="text-sm text-gray-700">{t("lb_place_work")}</h6>
+      </div>
+      <div className="lg:flex">
+        <div className="mt-8 lg:w-2/4">
+          <TextField label={t("lb_street_name_number")} {...formMethods.register("addressLine1")} />
         </div>
-        <div className="mt-8 lg:mt-2">
-          <Label>{t("lb_biography")}</Label>
-          <Editor
-            //check with Emil about Hint, use paragraph?
-            getText={() => md.render(formMethods.getValues("biography") || "")}
-            setText={(value: string) => {
-              formMethods.setValue("biography", turndownService.turndown(value), { shouldDirty: true });
-            }}
-            excludedToolbarItems={["blockType"]}
-          />
+        <div className="mt-8 lg:ml-2 lg:w-2/4">
+          <TextField label={t("lb_zip_code")} {...formMethods.register("zip")} />
         </div>
-        <div className="mt-8">
-          <h4 className="text-lg font-bold">{t("lb_work_address")}</h4>
-          <h6 className="text-sm text-gray-700">{t("lb_place_work")}</h6>
-        </div>
-        <div className="lg:flex">
-          <div className="mt-8 lg:w-2/4">
-            <TextField label={t("lb_street_name_number")} {...formMethods.register("street_name_number")} />
-          </div>
-          <div className="mt-8 lg:ml-2 lg:w-2/4">
-            <TextField label={t("lb_zip_code")} {...formMethods.register("zip_code")} />
-          </div>
-        </div>
-        <div className="mt-8 lg:mt-2">
-          <TextField label={t("lb_city")} {...formMethods.register("city")} />
-        </div>
+      </div>
+      <div className="mt-8 lg:mt-2">
+        <TextField label={t("lb_city")} {...formMethods.register("city")} />
+      </div>
 
-        <div>
-          <Checkbox description={t("lb_work_equals_billing_address")} />
-          {/* 
+      {/*what logic to implement here? maybe the one commented below*/}
+      <div>
+        <Checkbox description={t("lb_work_equals_billing_address")} />
+        {/* 
         defaultChecked={form.getValues(`steps.${step.stepNumber - 1}.numberRequired`) || false
         }          
         onChange={(e) =>form.setValue(`steps.${step.stepNumber - 1}.numberRequired`, e.target.checked)
         } */}
-        </div>
+      </div>
 
-        <div className="mt-8">
-          <h4 className="text-lg font-bold">{t("lb_specialities")}</h4>
-          <h6 className="text-sm text-gray-700">{t("lb_specialities_hint")}</h6>
-        </div>
+      <div className="mt-8">
+        <h4 className="text-lg font-bold">{t("lb_specialities")}</h4>
+        <h6 className="text-sm text-gray-700">{t("lb_specialities_hint")}</h6>
+      </div>
 
-        <div className=" lg:flex">
-          <div className="mt-8 lg:w-2/4">
-            <TextField label={t("lb_to_be_inputed_later")} {...formMethods.register("to_be_inputed_later")} />
-          </div>
-          <div className="mt-8 lg:ml-2 lg:w-2/4">
-            <TextField label={t("lb_to_be_inputed_later")} {...formMethods.register("to_be_inputed_later")} />
-          </div>
-        </div>
-        <div className="mt-8 lg:mt-2">
-          <TextField label={t("lb_to_be_inputed_later")} {...formMethods.register("to_be_inputed_later")} />
-        </div>
+      <div className="mt-8">
+        <h4 className="text-lg font-bold">{t("lb_place_work")}</h4>
+      </div>
 
-        <div className="mt-8">
-          <h4 className="text-lg font-bold">{t("lb_place_work")}</h4>
-        </div>
-
-        <div className="mt-2 font-bold lg:flex">
-          <Button StartIcon={HomeIcon} color="secondary" type="submit">
-            {t("lb_home")}
-          </Button>
-          <Button className="ml-10" StartIcon={DesktopComputerIcon} color="secondary" type="submit">
-            {t("lb_online")}
-          </Button>
-        </div>
-        <div className=" text-right">
-          {" "}
-          <Button
-            className=" mt-8 bg-green-700"
-            StartIcon={CheckIcon}
-            disabled={isDisabled}
-            color="primary"
-            type="submit">
-            {t("save")}
-          </Button>
-        </div>
-      </Form>
-    </Shell>
+      <div className="mt-2 font-bold lg:flex">
+        <Button StartIcon={HomeIcon} color="secondary" type="submit">
+          {t("lb_home")}
+        </Button>
+        <Button className="ml-10" StartIcon={DesktopComputerIcon} color="secondary" type="submit">
+          {t("lb_online")}
+        </Button>
+      </div>
+      <div className="text-right">
+        <Button disabled={isDisabled} color="primary" className="mt-8" type="submit">
+          {t("update")}
+        </Button>
+      </div>
+    </Form>
   );
-};
-
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  const ssr = await ssrInit(context);
-
-  return {
-    props: {
-      trpcState: ssr.dehydrate(),
-    },
-  };
 };
 
 export default InformationPage;
---
+++ packages/prisma/migrations/20230214123159_add_company_address_to_user/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "addressLine1" TEXT,
+ADD COLUMN     "addressLine2" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "companyName" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "zip" TEXT;
