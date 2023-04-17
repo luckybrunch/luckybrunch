96cc27ec78c1b93487b0f61843059400a1fe4559
Ertan Kara <kara.ertan9@gmail.com>
2023-04-17 10:56:27 +0300

    Remove table keyword (#33)

--- packages/prisma/migrations/20230413084550_add_user_type_enum/migration.sql
+++ packages/prisma/migrations/20230413084550_add_user_type_enum/migration.sql
@@ -4,6 +4,6 @@ CREATE TYPE "CustomerType" AS ENUM ('COACH', 'CUSTOMER');
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "customerType" "CustomerType" DEFAULT 'COACH';
 
-UPDATE TABLE "users" SET "customerType" = 'COACH';
+UPDATE "users" SET "customerType" = 'COACH';
 
 ALTER TABLE "users" ALTER COLUMN "customerType" SET NOT NULL;
