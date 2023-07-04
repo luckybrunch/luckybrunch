f21d9566ddfb133dbe7561a0c497f1e7e3ff27ee
Ertan Kara <kara.ertan9@gmail.com>
2023-07-04 12:52:18 +0200

    Inform user when client list is empty (#88)

--- apps/web/pages/clients.tsx
+++ apps/web/pages/clients.tsx
@@ -4,8 +4,17 @@ import { generateChannelName } from "@calcom/features/chat/lib/generateChannelNa
 import Shell from "@calcom/features/shell/Shell";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
-import { SkeletonLoader, List, ListItemTitle, ListItemText, ListItem, Badge } from "@calcom/ui";
-import { FiLink } from "@calcom/ui/components/icon";
+import {
+  SkeletonLoader,
+  List,
+  ListItemTitle,
+  ListItemText,
+  ListItem,
+  Badge,
+  EmptyScreen,
+  Button,
+} from "@calcom/ui";
+import { FiFile, FiLink } from "@calcom/ui/components/icon";
 
 import { withQuery } from "@lib/QueryCell";
 import useMeQuery from "@lib/hooks/useMeQuery";
@@ -23,6 +32,14 @@ export default function Clients() {
       subtitle={t("lb_client_list_subtitle")}>
       <WithQuery
         customLoader={<SkeletonLoader />}
+        empty={() => (
+          <EmptyScreen
+            Icon={FiFile}
+            headline={t("lb_client_list_empty_title")}
+            description={t("lb_client_list_empty_description")}
+            buttonRaw={<Button href="/dashboard">{t("lb_client_list_empty_action")}</Button>}
+          />
+        )}
         success={({ data: clients }) => {
           return (
             <List className="border-none" role="list">
