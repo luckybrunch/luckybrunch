7d6969d944837399412e5dfc4259ad5a85f45f24
Emil <emil@twigbit.com>
2024-07-10 11:01:36 +0200

    Fix layout on dashboard diff

--- packages/features/coaches/components/ProfileDiff.tsx
+++ packages/features/coaches/components/ProfileDiff.tsx
@@ -28,7 +28,9 @@ type ProfileDiffProps = {
 const NewAddition = (props: Pick<FieldDiffMetada, "newValue">) => {
   return (
     <>
-      <Badge variant="lb_green">{props.newValue}</Badge>
+      <Badge className="h-auto" variant="lb_green">
+        {props.newValue}
+      </Badge>
     </>
   );
 };
@@ -36,9 +38,13 @@ const NewAddition = (props: Pick<FieldDiffMetada, "newValue">) => {
 const Change = (props: Pick<FieldDiffMetada, "oldValue" | "newValue">) => {
   return (
     <>
-      <Badge variant="lb_green">{props.oldValue}</Badge>
+      <Badge className="h-auto" variant="lb_green">
+        {props.oldValue}
+      </Badge>
       <FiArrowRight className="inline-block h-[20px] w-[30px] px-1" />
-      <Badge variant="lb_green">{props.newValue}</Badge>
+      <Badge className="h-auto" variant="lb_green">
+        {props.newValue}
+      </Badge>
     </>
   );
 };
@@ -46,7 +52,7 @@ const Change = (props: Pick<FieldDiffMetada, "oldValue" | "newValue">) => {
 const Deletion = (props: Pick<FieldDiffMetada, "oldValue">) => {
   return (
     <>
-      <Badge variant="red" withDot={true}>
+      <Badge className="h-auto" variant="red" withDot={true}>
         {props.oldValue}
       </Badge>
     </>
@@ -60,19 +66,20 @@ export function ProfileDiff(props: ProfileDiffProps) {
 
   return (
     <ListItem rounded={false} className="block flex-col border-0 md:border-0">
-      <div className="flex items-center justify-between">
-        <div className="flex max-w-[65%] flex-col p-1">
+      <div className="flex flex-wrap items-center justify-between">
+        <div className="flex max-w-[65%] flex-col items-start p-1">
           <h3 className="mb-1 text-sm font-medium capitalize text-neutral-900">
             {t(translationKeys[field])}
           </h3>
-          <div>
-            {isNew && <NewAddition newValue={newValue} />}
-            {isDeleted && <Deletion oldValue={oldValue} />}
-            {!isDeleted && !isNew && <Change oldValue={oldValue} newValue={newValue} />}
-          </div>
+
+          {isNew && <NewAddition newValue={newValue} />}
+          {isDeleted && <Deletion oldValue={oldValue} />}
+          {!isDeleted && !isNew && <Change oldValue={oldValue} newValue={newValue} />}
         </div>
         <Button
+          className="ml-auto shrink-0 self-end rounded-md text-xs"
           color="secondary"
+          size="sm"
           disabled={isLoading}
           StartIcon={FiArrowLeft}
           onClick={() => {
